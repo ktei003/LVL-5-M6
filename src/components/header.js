e0a97124a883520353ed23ce63e6d351e8d51f6b
keanu teina <keanut@missionreadyhq.com>
2021-11-28 04:37:36 +1300

    attempt number 2

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import 'styles/header.css';
+
+const Header = () => {
+	return (
+        <div>
+            <h1 className="test">header</h1>
+        </div>
+    )
+    }
+
+export default Header;
